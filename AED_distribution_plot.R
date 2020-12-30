701d8fe9a9d07d02fed2ae9448e3fa78f5a3c507
Qionghou Li <32695830+LQHHHHH@users.noreply.github.com>
2020-12-30 10:53:06 +0800

    add file and example file

---
+++ AED_distribution_plot.R
@@ -0,0 +1,27 @@
+## plot AED
+## grep '>' *maker.proteins.fasta | awk '{print $1":"$4}' | perl -ne '@array=split(":", $_); @name=split("-",$array[0]);print $name[1]."\t".$array[2];' > eAED_statistics*
+setwd("")
+rm(list=ls())
+
+## Frequency function
+get_Freq <- function(table2check) {
+  
+  breaks = seq(0, 1, by=0.02) 
+  AED_table <- table(table2check)
+  AED_frame <- as.data.frame(AED_table)
+  AED.cut = cut(table2check$V2, breaks, right=FALSE) 
+  AED.freq = table(AED.cut)
+  cumfreq = c(0, cumsum(AED.freq)/sum(AED.freq)) 
+  
+  return(cumfreq)  
+}
+
+## file1
+file2check <- "Aed.output"
+AED1 <- read.table(file2check)
+mean_AED1 = mean(AED1$V2)
+median_AED1 = median(AED1$V2)
+cumfreq_Ret1 <- get_Freq(AED1)
+
+qplot(breaks,cumfreq_Ret1,geom='line',ylab="Cummulative Fraction of Annotation", xlab="AED")+
+  theme_bw()
